b2525503b6d4ec107a9e933068e8fa3d8b051dad
Turar Abu <mail@turarabu.me>
2020-12-18 16:31:54 +0600

    add content

--- frontend/main/script/data/content.js
+++ frontend/main/script/data/content.js
@@ -1,5 +1,3 @@
-const list = []
-
 export default {
   state: {
     poster: {
@@ -7,8 +5,8 @@ export default {
       en: {}
     },
     video: {
-      ru: getFor('rus'),
-      en: getFor('eng')
+      ru: getForeng(),
+      en: getForEng()
     }
   },
 
@@ -19,15 +17,32 @@ export default {
 
 
 // state
-function getFor (lang) {
-  const result = []
-  
-  list.forEach(video => {
-    const path = video.replace('%lang%', lang)
-    result.push(path)
-  })
+function getForeng () {
+  return [
+    '/video/rus/001-video.mp4', '/video/rus/002-video.mp4', '/video/rus/003-video.mp4',
+    '/video/rus/004-video.mp4', '/video/rus/005-video.mp4', '/video/rus/006-video.mp4',
+    '/video/rus/007-video.mp4', '/video/rus/008-video.mp4', '/video/rus/009-video.mp4',
+    '/video/rus/010-video.mp4', '/video/rus/011-video.mp4', '/video/rus/012-video.mp4',
+    '/video/rus/013-video.mp4', '/video/rus/014-video.mp4', '/video/rus/015-video.mp4',
+    '/video/rus/016-video.mp4', '/video/rus/017-video.mp4', '/video/rus/018-video.mp4',
+    '/video/rus/019-video.mp4', '/video/rus/020-video.mp4', '/video/rus/021-video.mp4',
+    '/video/rus/022-video.mp4', '/video/rus/023-video.mp4', '/video/rus/024-video.mp4',
+    '/video/rus/025-video.mp4', '/video/rus/026-video.mp4', '/video/rus/027-video.mp4',
+    '/video/rus/028-video.mp4'
+  ]
+}
 
-  return result
+function getForEng () {
+  return [
+    '/video/eng/001-video.mp4', '/video/eng/002-video.mp4', '/video/eng/003-video.mp4',
+    '/video/eng/004-video.mp4', '/video/eng/005-video.mp4', '/video/eng/006-video.mp4',
+    '/video/eng/007-video.mp4', '/video/eng/008-video.mp4', '/video/eng/009-video.mp4',
+    '/video/eng/010-video.mp4', '/video/eng/011-video.mp4', '/video/eng/012-video.mp4',
+    '/video/eng/013-video.mp4', '/video/eng/014-video.mp4', '/video/eng/015-video.mp4',
+    '/video/eng/016-video.mp4', '/video/eng/017-video.mp4', '/video/eng/018-video.mp4',
+    '/video/eng/019-video.mp4', '/video/eng/020-video.mp4', '/video/eng/021-video.mp4',
+    '/video/eng/022-video.mp4', '/video/eng/023-video.mp4', '/video/eng/024-video.mp4'
+  ]
 }
 
 
